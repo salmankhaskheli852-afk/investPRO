ecef49a5b3739fec29a2d6934fd5634514d8aef6
Salman Khaskheli <salmankhaskheli852@gmail.com>
2025-12-21 21:47:24 +0000

    Storage parhao

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:329778538170:web:67c77e8ff88ea982b9c483",
   "apiKey": "AIzaSyA4Z0B7FY97RjaJ5WVJ4ProbOu5bewWQV4",
   "authDomain": "studio-8394318664-34f21.firebaseapp.com",
+  "storageBucket": "studio-8394318664-34f21.appspot.com",
   "measurementId": "",
   "messagingSenderId": "329778538170"
 };
