4b9f42f3f9b93b47f28f62ee647ada0b1ddb6c54
Salman Khaskheli <salmankhaskheli852@gmail.com>
2025-12-27 09:12:58 +0000

    ye scrole left right nhi ho raha he  (_for element <Primitive.div>_)

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -15,7 +15,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-12 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
+      "flex h-12 items-center justify-start rounded-md bg-muted p-1 text-muted-foreground",
       className
     )}
     {...props}
