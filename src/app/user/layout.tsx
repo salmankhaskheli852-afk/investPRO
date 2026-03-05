199c726edd8f5ed8ecb89ea13ab776188256745d
Salman Khaskheli <salmankhaskheli852@gmail.com>
2026-03-05 09:22:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -54,11 +54,11 @@ export default function UserLayout({
 
   return (
     <div className="bg-background min-h-screen w-full">
-      <div className="relative mx-auto w-full min-h-screen flex flex-col">
+      <div className="mx-auto w-full min-h-screen flex flex-col">
         <Header />
         <main className="flex-1 p-4 sm:p-6 lg:p-8 pb-24 w-full">
-          {/* Poori screen (Landscape) view ke liye max-w-none rakha gaya hai */}
-          <div className="w-full max-w-none mx-auto">
+          {/* Poori screen (Landscape) view ke liye max-width ki limit khatam kar di gayi hai */}
+          <div className="w-full mx-auto">
             {showVerificationPopup && <VerificationPopup />}
             <ReferralRequestManager />
             {children}
